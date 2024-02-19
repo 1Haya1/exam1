6a86adc8aafa4a85b8174d92c3168017f34836a8
Haya <154345101+1Haya1@users.noreply.github.com>
2024-02-19 11:52:27 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.lang.Math.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        //EX1
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Enter firstnum");
+//        int num1 = s.nextInt();
+//
+//        System.out.println("Enter secondnum");
+//        int num2 = s.nextInt();
+//
+//        System.out.println("Enter thirdnum");
+//        int num3 = s.nextInt();
+//
+//        int largestnum = Math.max(num1,num2,num3);
+//
+//        System.out.println("the largest number is"+largestnum);
+
+
+//    }}
+
+
+        //EX2
+
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Enter a word");
+//        String word = s.nextLine();
+//
+//        System.out.println("Enter a number to search");
+//        int num = s.nextInt();
+//
+//        for (v: word.charAt(num){
+//            char index;
+//        index = word.charAt();
+//        System.out.printf("character is"+index);
+//        }}}
+
+
+        //EX3
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("enter -1 to end");
+//
+//        System.out.println("Enter firstnum");
+//        int num = s.nextInt();
+//        int sum = 0;
+//        sum += num;
+//
+//        if (num == -1){
+//            break;
+//        }else{
+//           System.out.println("sum"+sum);}}}
+
+
+        //EX4
+//        int[] num={10, -21 , 30, 31, -25};
+//
+//           if (num>0){
+//            System.out.println("its positive");
+//    } else if (num<0) {
+//               System.out.println("its negative");
+//           }else{
+//               System.out.printf("its zero");
+//        }}}}
+
+
+        //EX5
+//    String[] tu={"Tuwaiq", "Bootcamp" , "Student” ,”JAVA"};
+//
+//
+
+
+
+
+
+
