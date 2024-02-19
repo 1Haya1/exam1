328732639c853c2855a73d7a929213679689c3b6
Haya <154345101+1Haya1@users.noreply.github.com>
2024-02-19 12:05:57 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-import static java.lang.Math.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -11,19 +10,28 @@ public class Main {
 
 
         //EX1
-//        Scanner s = new Scanner(System.in);
-//        System.out.println("Enter firstnum");
-//        int num1 = s.nextInt();
+//         Scanner scanner = new Scanner(System.in);
+        
+//        System.out.println("Enter the first number:");
+//        double num1 = scanner.nextDouble();
 //
-//        System.out.println("Enter secondnum");
-//        int num2 = s.nextInt();
+//        System.out.println("Enter the second number:");
+//        double num2 = scanner.nextDouble();
 //
-//        System.out.println("Enter thirdnum");
-//        int num3 = s.nextInt();
+//        System.out.println("Enter the third number:");
+//        double num3 = scanner.nextDouble();
+
+//        double largestNumber = num1;
 //
-//        int largestnum = Math.max(num1,num2,num3);
+//        if (num2 > largestNumber) {
+//            largestNumber = num2;
+//        }
 //
-//        System.out.println("the largest number is"+largestnum);
+//        if (num3 > largestNumber) {
+//            largestNumber = num3;
+//        }
+//        System.out.println("The largest number is: " + largestNumber);
+
 
 
 //    }}
@@ -31,51 +39,88 @@ public class Main {
 
         //EX2
 
-//        Scanner s = new Scanner(System.in);
-//        System.out.println("Enter a word");
-//        String word = s.nextLine();
-//
-//        System.out.println("Enter a number to search");
-//        int num = s.nextInt();
-//
-//        for (v: word.charAt(num){
-//            char index;
-//        index = word.charAt();
-//        System.out.printf("character is"+index);
-//        }}}
+//            Scanner scanner = new Scanner(System.in);
+
+//        System.out.println("Enter a string:");
+//        String inputString = scanner.nextLine();
+
+//        System.out.println("Enter the index of the character you want to retrieve:");
+//        int index = scanner.nextInt();
+
+//        if (index >= 0 && index < inputString.length()) {
+//            char characterAtIndex = inputString.charAt(index);
+//            System.out.println("The character at index " + index + " is: " + characterAtIndex);
+//        } else {
+//            System.out.println("Invalid index. The string length is " + inputString.length() + ".");
+//        }
 
 
         //EX3
-//        Scanner s = new Scanner(System.in);
-//        System.out.println("enter -1 to end");
-//
-//        System.out.println("Enter firstnum");
-//        int num = s.nextInt();
+//          Scanner scanner = new Scanner(System.in);
+
 //        int sum = 0;
-//        sum += num;
+//        boolean continueInput = true;
 //
-//        if (num == -1){
-//            break;
-//        }else{
-//           System.out.println("sum"+sum);}}}
+//        while (continueInput) {
+//            System.out.print("Enter a number (enter 0 to finish): ");
+//            int number = scanner.nextInt();
+//
+//            if (number != 0) {
+//                sum += number; // Add the entered number to the sum
+//            } else {
+//                continueInput = false; // Stop the loop if the user enters 0
+//            }
+//        }
+//        System.out.println("The sum of the numbers entered is: " + sum);
 
 
         //EX4
-//        int[] num={10, -21 , 30, 31, -25};
+//       int[] array = {10, -21, 30, 31, -25};
 //
-//           if (num>0){
-//            System.out.println("its positive");
-//    } else if (num<0) {
-//               System.out.println("its negative");
-//           }else{
-//               System.out.printf("its zero");
-//        }}}}
+//        System.out.println("Original Array:");
+//        printArray(array);
+//
+//        System.out.println("Expected Output:");
+//
+//        for (int num : array) {
+//            if (num >= 0) {
+//                System.out.println(num + " is a positive number");
+//            } else {
+//                System.out.println(num + " is a negative number");
+//            }
+//        }
+//    }
+//    public static void printArray(int[] arr) {
+//        System.out.print("[");
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.print(arr[i]);
+//            if (i < arr.length - 1) {
+//                System.out.print(", ");
+//            }
+//        }
+//        System.out.println("]");
 
 
         //EX5
-//    String[] tu={"Tuwaiq", "Bootcamp" , "Student” ,”JAVA"};
-//
-//
+// //   String[] words = {"Tuwaiq", "Bootcamp", "Student", "JAVA"};
+
+//         String shortestWord = findShortestWord(words);
+
+//         System.out.println("Shortest word: " + shortestWord);
+//     }
+//     public static String findShortestWord(String[] words) {
+//         if (words == null || words.length == 0) {
+//             return null;
+//         }
+//         String shortestWord = words[0]; 
+
+//         for (int i = 1; i < words.length; i++) {
+//             if (words[i].length() < shortestWord.length()) {
+//                 shortestWord = words[i];
+//             }
+//         }
+
+//         return shortestWord;
 
 
 
